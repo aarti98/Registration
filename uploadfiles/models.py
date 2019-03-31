eaf74f0a0b207a0c4526c0e8175d0174d3617df2
aa <aj@gmail.com>
2019-03-31 20:13:02 +0530

    Models updated

--- uploadfiles/models.py
+++ uploadfiles/models.py
@@ -6,7 +6,7 @@ class Document(models.Model):
     description = models.CharField(max_length=200, blank=True)
     document    = models.FileField(upload_to='documents')
     uploaded_at = models.DateTimeField('Time',auto_now_add=True)
-    user        = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user        = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.description
