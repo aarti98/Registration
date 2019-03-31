caa1aa3f57714a8ceccf706ed35dda72644f3fb8
Jaipreet Singh <jaipreet.singh@alayacare.com>
2019-03-31 11:19:05 -0400

    Fixing structure

--- uploadfiles/views.py
+++ uploadfiles/views.py
@@ -1,7 +1,9 @@
-from .models import Document
 from django.http import Http404
 from django.conf import settings
 from .forms import DocumentForm
+from uploadfiles.models import Document
+from django.views.generic import ListView
+
 from django.shortcuts import render,redirect
 
 User = settings.AUTH_USER_MODEL
@@ -29,3 +31,21 @@ def upload_file(request):
 def display_file(request):
     files = Document.objects.all()
     return render(request, 'uploadfiles/display.html', {'files': files})
+
+
+class SearchView(ListView):
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchView, self).get_context_data(*args,**kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        query = request.GET.get('q')
+
+        if query is not None:
+            return Document.objects.search(query)
+
+        return Document.objects.none()
